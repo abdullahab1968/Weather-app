47ba044b4d68ee16e6881f51ff944fff8ba9823b
Abdallah Abu khdair <71616389+abdullahab1968@users.noreply.github.com>
2023-03-17 01:15:16 +0200

    create server file

---
+++ server.js
@@ -0,0 +1,19 @@
+const express = require('express')
+const app = express()
+const api = require('./server/routes/api')
+
+app.use(express.json())
+app.use(express.urlencoded({ extended: false }))
+// app.use(express.static(path.join(__dirname, 'node_modules')))
+
+const mongoose = require('mongoose')
+mongoose.connect("mongodb://127.0.0.1:27017/weatherDB", {
+  useNewUrlParser: true,
+}).catch((err)=> console.log(err))
+
+app.use('/', api)
+
+const port = 3001
+app.listen(port, function () {
+    console.log(`Running on port ${port}`)
+})
