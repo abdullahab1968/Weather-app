7f6fd0f0a269dcb4686244d4932cdf287df3e9f4
Abdallah Abu khdair <71616389+abdullahab1968@users.noreply.github.com>
2023-03-17 01:07:07 +0200

    build weather DB Schema

---
+++ server/routes/api.js
@@ -0,0 +1,11 @@
+const mongoose = require('mongoose')
+const Schema = mongoose.Schema
+const weatherSchema = new Schema({
+    name:String,
+    temperature: Number,
+    condition: String,
+    conditionPic: String
+})
+
+const Weather = mongoose.model("Weather", weatherSchema)
+module.exports = Weather
