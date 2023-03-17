b4de4ab40a825e4a4098a301a5e9afd4b8adb0eb
Abdallah Abu khdair <71616389+abdullahab1968@users.noreply.github.com>
2023-03-17 02:57:00 +0200

    weather route bug in sending request

--- server.js
+++ server.js
@@ -8,7 +8,8 @@ app.use(express.urlencoded({ extended: false }))
 
 const mongoose = require('mongoose')
 mongoose.connect("mongodb://127.0.0.1:27017/weatherDB", {
-  useNewUrlParser: true,
+    useNewUrlParser: true,
+    useUnifiedTopology: true
 }).catch((err)=> console.log(err))
 
 app.use('/', api)
--- server/routes/api.js
+++ server/routes/api.js
@@ -1,11 +1,37 @@
-const mongoose = require('mongoose')
-const Schema = mongoose.Schema
-const weatherSchema = new Schema({
-    name:String,
-    temperature: Number,
-    condition: String,
-    conditionPic: String
-})
+const WEATHER = {
+    geocodeURL : "http://api.openweathermap.org/geo/1.0/direct",
+    apiKey: '32140e6577fd0f3a4b0cbbcff1ed4dd1',
+    weatherURL: "https://api.openweathermap.org/data/2.5/weather"
 
-const Weather = mongoose.model("Weather", weatherSchema)
-module.exports = Weather
+}
+const express = require("express");
+const router = express.Router();
+const axios = require('axios')
+
+router.get('/weather', function(req, res){
+    const city = req.query.city
+    axios.get (WEATHER.geocodeURL, {
+        params: {
+            q: city,
+            limit: 1,
+            appid : WEATHER.apiKey
+        }
+    }).then(response => {
+        const lat = response.data[0].lat
+        const lon = response.data[0].lon
+        return [lat, lon]
+
+    }).catch((err) => console.log(err))
+    .then(coordinates => {
+        axios.get(WEATHER.weatherURL, {
+            params: {
+                lat: coordinates[0],
+                lon: coordinates[1],
+                appid: WEATHER.apiKey
+            }
+        })
+    }).catch((err) => console.log(err))
+    .then(weatherData => res.send(weatherData))
+    .catch((err) => console.log(err))
+})
+module.exports = router
