4f84e6fbcd75fe4af78bbb60b46e28a4b8f1c60b
Abdallah Abu khdair <71616389+abdullahab1968@users.noreply.github.com>
2023-03-17 18:51:38 +0200

    add new city ,delete city, find city routes from the DB

--- server/models/Weather.js
+++ server/models/Weather.js
@@ -1,8 +1,8 @@
 const mongoose = require('mongoose')
 const Schema = mongoose.Schema
 const weatherSchema = new Schema({
-    name:String,
-    temperature: Number,
+    cityName:String,
+    temperature: String,
     condition: String,
     conditionPic: String
 })
--- server/routes/api.js
+++ server/routes/api.js
@@ -1,26 +1,49 @@
-const WEATHER = {
-    geocodeURL : "http://api.openweathermap.org/geo/1.0/direct",
+const URL = {
     apiKey: '32140e6577fd0f3a4b0cbbcff1ed4dd1',
     weatherURL: "https://api.openweathermap.org/data/2.5/weather"
 
 }
+const Weather = require('../models/Weather')
 const express = require("express");
 const router = express.Router();
 const axios = require('axios')
 
-router.get('/weather', function(req, res){
+router.get('/weather/api', function(req, res){
     const city = req.query.city
-    return axios.get (WEATHER.weatherURL, {
+    return axios.get (URL.weatherURL, {
         params: {
             q: city,
-            appid : WEATHER.apiKey,
+            appid : URL.apiKey,
             units: 'metric'
-
         }
     })
     .then(response => {
         res.send(response.data)
     })
     .catch((err) => res.send(err))
+})
+router.get('/weather', function(req, res){
+
+    Weather.find({cityName: req.query.city})
+            .then(cityData => {
+                res.send(cityData)
+            })
+            .catch(err => res.send({'Error message': err}))
+
+})
+router.post('weather/add', function(req, res){
+    const newCity = req.query.city
+    const weather = new Weather({
+        cityName: newCity.cityName,
+        temperature: newCity.temperature,
+        condition: newCity.condition,
+        conditionPic: newCity.conditionPic
+
+    })
+    weather.save()
+            .then(() => res.end())
+})
+router.delete('/weather/delete', function(req, res){
+    Weather.find({cityName: req.query.city}).remove()
 })
 module.exports = router
